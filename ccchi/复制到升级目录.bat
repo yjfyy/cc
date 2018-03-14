7549e5e39f39961a04474adf66f9b8a82fa83046
yeyu <yjfyeyu@gmail.com>
2018-03-14 21:26:44 +0800

    1

--- ccchi/复制到升级目录.bat
+++ ccchi/复制到升级目录.bat
@@ -1,6 +1,8 @@
 copy .\chg\lang_chg.mix E:\Projects\updatasys\cnc1\patch\temp
 copy .\chn\lang_chn.mix E:\Projects\updatasys\cnc1\patch\temp
 copy .\chm\lang_chm.mix E:\Projects\updatasys\cnc1\patch\temp
-copy .\ccconfigchg.lan E:\Projects\updatasys\cnc1\patch\temp
-copy .\ccconfigchn.lan E:\Projects\updatasys\cnc1\patch\temp
-copy .\ccconfigchm.lan E:\Projects\updatasys\cnc1\patch\temp
+copy .\chv\lang_chv.mix E:\Projects\updatasys\cnc1\patch\temp
+copy .\ccconfigchg.lan E:\Projects\updatasys\cnc1\patch\temp\ccconfigchg.lan
+copy .\ccconfigchg.lan E:\Projects\updatasys\cnc1\patch\temp\ccconfigchn.lan
+copy .\ccconfigchg.lan E:\Projects\updatasys\cnc1\patch\temp\ccconfigchm.lan
+copy .\ccconfigchg.lan E:\Projects\updatasys\cnc1\patch\temp\ccconfigchv.lan
