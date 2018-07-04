b6e0197b1b1368ff80066404c7f56c186c45c0ae
yjfyy <yjfyeyu@gmail.com>
2018-07-04 17:15:17 +0800

    1

--- ra1/发布.bat
+++ ra1/发布.bat
@@ -1,6 +1,2 @@
-copy conquer.eng "F:\projects\updatasys\WestWood_Classic\temp\RA"
-copy hires.mix "F:\projects\updatasys\WestWood_Classic\temp\RA"
-copy local.mix "F:\projects\updatasys\WestWood_Classic\temp\RA"
-copy tutorial.ini "F:\projects\updatasys\WestWood_Classic\temp\RA"
-copy mission.ini "F:\projects\updatasys\WestWood_Classic\temp\RA"
+copy copy ".\germanlanguagepack.mix-2-ÃË¾üÈÎÎñ\germanlanguagepack.mix" "F:\projects\updatasys\WestWood_Classic\temp\RA"
 pause
