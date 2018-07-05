d8d2ada8412fda46afe2b1521b112a8d4124f55f
yjfyy <yjfyeyu@gmail.com>
2018-07-05 15:47:25 +0800

    1.29

--- ra1/发布.bat
+++ ra1/发布.bat
@@ -1,2 +1,3 @@
-copy copy ".\germanlanguagepack.mix-2-盟军任务\germanlanguagepack.mix" "F:\projects\updatasys\WestWood_Classic\temp\RA"
+copy ".\germanlanguagepack.mix-2-盟军任务\germanlanguagepack.mix" "F:\projects\updatasys\WestWood_Classic\temp\RA"
+copy ".\germancensoredlanguagepack.MIX-3苏军任务\germancensoredlanguagepack.mix" "F:\projects\updatasys\WestWood_Classic\temp\RA"
 pause
