7b4342cdddeb89408c8e92c6a4707d62bb2b8bbc
yjfyy <yjfyeyu@gmail.com>
2018-07-05 15:32:07 +0800

    1.29

--- ra1/germancensoredlanguagepack.MIX-3苏军任务/测试.bat
+++ ra1/germancensoredlanguagepack.MIX-3苏军任务/测试.bat
@@ -1,5 +1,6 @@
 copy "F:\projects\cc\ra1\germanlanguagepack.mix-2-盟军任务\germanlanguagepack.mix" "D:\games\WestWood_Classic\RA"
 copy "F:\projects\cc\ra1\germancensoredlanguagepack.MIX-3苏军任务\germancensoredlanguagepack.mix" "D:\games\WestWood_Classic\RA"
-cd "D:\games\WestWood_Classic"
+cd "D:\games\WestWood_Classic\ra"
 d:
-"西木经典游戏汉化纪念版.exe"
+ra95.exe
+rem "西木经典游戏汉化纪念版.exe"
