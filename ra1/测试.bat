179cd6b84dc5825aeef005f4582f0bcd0056408e
yjfyeyu <yjfyeyu@gmail.com>
2019-01-22 16:42:46 +0800

    1.44

--- ra1/测试.bat
+++ ra1/测试.bat
@@ -1,5 +1,6 @@
 copy ".\germanlanguagepack.mix-2-盟军任务\germanlanguagepack.mix" "D:\games\WestWood_Classic\RA"
 copy ".\germancensoredlanguagepack.MIX-3苏军任务\germancensoredlanguagepack.mix" "D:\games\WestWood_Classic\RA"
+copy ".\germanuncensoredlanguagepack.MIX-4-遭遇战\germanuncensoredlanguagepack.mix" "D:\games\WestWood_Classic\RA"
 cd "D:\games\WestWood_Classic"
 d:
 西木经典游戏汉化纪念版.exe"
