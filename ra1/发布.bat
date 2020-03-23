6d8273caefcb1e61aed2f48e581e523fecb872ff
yjfyy <yjfyeyu@gmail.com>
2020-03-23 23:17:08 +0800

    修正了一些遗漏

--- ra1/发布.bat
+++ ra1/发布.bat
@@ -2,5 +2,6 @@ copy ".\frenchlanguagepack.MIX-5资料片\cs.mix" "..\WestWood_Classic\WestWood_Cla
 copy ".\germancensoredlanguagepack.MIX-3苏军任务\soviet.mix" "..\WestWood_Classic\WestWood_Classic\Resources"
 copy ".\germanlanguagepack.mix-2-盟军任务\allied.mix" "..\WestWood_Classic\WestWood_Classic\Resources"
 copy ".\germanuncensoredlanguagepack.MIX-4-遭遇战\skirmish.mix" "..\WestWood_Classic\WestWood_Classic\Resources"
-copy ".\russianlanguagepack.MIX-7余生资料片苏军\skirmish.mix" "..\WestWood_Classic\WestWood_Classic\Resources"
+copy ".\russianlanguagepack.MIX-7余生资料片苏军\am_soviet.mix" "..\WestWood_Classic\WestWood_Classic\Resources"
 copy ".\spanishlanguagepack.MIX-6余生资料片\am_allied.mix" "..\WestWood_Classic\WestWood_Classic\Resources"
+pause
